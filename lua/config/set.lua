143548002f05d05e5ea22082e1153ef472effe0b
soheyl <soheylrashidsahebjam@gmail.com>
2025-03-04 12:35:44 +0330

    hello

--- lua/config/set.lua
+++ lua/config/set.lua
@@ -1,41 +1,44 @@
+local set = vim.keymap.set
+local opt = vim.opt
+
 vim.g.mapleader = " "
-vim.opt.guicursor = ""
+opt.guicursor = ""
 
-vim.keymap.set("n", "<C-n>", ":Ex<cr>",{ silent = true })
+set("n", "<C-n>", ":Ex<cr>",{ silent = true })
 
-vim.opt.number = true
-vim.opt.relativenumber = true
+opt.number = true
+opt.relativenumber = true
 
-vim.opt.wrap = false
+opt.wrap = false
 
-vim.opt.smartindent = true
+opt.smartindent = true
 
-vim.opt.shiftwidth = 4
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.expandtab = true
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+opt.swapfile = false
+opt.backup = false
+opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+opt.undofile = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+opt.hlsearch = false
+opt.incsearch = true
 
-vim.opt.termguicolors = true
+opt.termguicolors = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
-vim.opt.isfname:append("@-@")
+opt.scrolloff = 8
+opt.signcolumn = "yes"
+opt.isfname:append("@-@")
 
-vim.opt.updatetime = 50
+opt.updatetime = 50
 
-vim.keymap.set("n", "J", "mzJ`z")
+set("n", "J", "mzJ`z")
 
-vim.keymap.set("n", "C-d", "<C-d>zz<cr>",{silent = true})
-vim.keymap.set("n", "C-u", "<C-u>zz<cr>",{silent = true})
-vim.keymap.set("n", "n", "nzzzv",{silent = true})
-vim.keymap.set("n", "N", "Nzzzv",{silent = true})
+set("n", "C-d", "<C-d>zz<cr>",{silent = true})
+set("n", "C-u", "<C-u>zz<cr>",{silent = true})
+set("n", "n", "nzzzv",{silent = true})
+set("n", "N", "Nzzzv",{silent = true})
 
-vim.keymap.set("x", "<leader>p", "\"_dP",{silent = true})
+set("x", "<leader>p", "\"_dP",{silent = true})
