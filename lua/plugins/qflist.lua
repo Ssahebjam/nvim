6cbeea1b9739479bcc6649fbd218799da44c6a15
soheyl <soheylrashidsahebjam@gmail.com>
2025-03-12 13:51:08 +0330

    d

---
+++ lua/plugins/qflist.lua
@@ -0,0 +1,14 @@
+return {
+  "kevinhwang91/nvim-bqf",
+  ft = "qf",
+  opts = {
+    auto_enable = true,
+    preview = {
+      auto_preview = true,    -- Preview shows when navigating with keys (no mouse)
+      win_height = 100,
+      win_vheight = 12,
+      border = "rounded",
+      show_title = false,
+    },
+  },
+}
