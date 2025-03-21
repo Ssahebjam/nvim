d54d0287133e9dd385f0108717e4c7c6cbee7de7
soheyl <soheylrashidsahebjam@gmail.com>
2025-03-21 12:52:43 +0330

    hello

---
+++ lua/plugins/vimBeGood/TODO.md
@@ -0,0 +1,1 @@
+#-- i want to make the rounds of the game to 20
---
+++ lua/plugins/vimBeGood/init.lua
@@ -0,0 +1,6 @@
+return {
+    "ThePrimeagen/vim-be-good",
+    config = function ()
+        vim.g["vim-be-good-round-count"] = 30
+    end,
+}
